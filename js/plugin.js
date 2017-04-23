0fe10924d118426f05f49e6351088b6ee266e526
lkneeshaw <lrkneeshaw@yahoo.com>
2017-04-22 17:55:43 -0700

    Got it functional
    - Added default styling
    - Added JavaScript
    - Styled hands

--- js/plugin.js
+++ js/plugin.js
@@ -1,4 +1,3 @@
 // A lot of what JS does is behind the scenes
 
 // Awesome Interaction
-console.log('Hello!');
--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,26 @@
 // A lot of what JS does is behind the scenes
 
 // Awesome Interaction
-console.log('Hello!');
+const secondHand = document.querySelector('.second-hand')
+const minHand = document.querySelector('.min-hand')
+const hourHand = document.querySelector('.hour-hand')
+
+function setDate() {
+    const now = new Date();
+    
+    const seconds = now.getSeconds();
+    const secondsDegrees = ((seconds / 60) * 360) + 90;
+    secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+    
+    const mins = now.getMinutes();
+    const minsDegrees = ((mins / 60) * 360) + 90;
+    minHand.style.transform = `rotate(${minsDegrees}deg)`;
+    
+    const hours = now.getHours();
+    const hoursDegrees = ((hours / 12) * 360) + 90;
+    hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
+    
+    console.log(hours)
+}
+
+setInterval(setDate, 1000);
